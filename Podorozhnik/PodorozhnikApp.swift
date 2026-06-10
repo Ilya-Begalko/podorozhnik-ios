6c0d4eb444975fbc3af143c562909ecb808381cd
i.begalko <i.begalko@topnlab.ru>
2026-06-10 21:26:21 +0300

    Initial commit

---
+++ Podorozhnik/PodorozhnikApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodorozhnikApp.swift
+//  Podorozhnik
+//
+//  Created by Илья Бегалко on 10.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct PodorozhnikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
